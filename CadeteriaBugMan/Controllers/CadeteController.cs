d19227945245d56dffeca2f95224a0631c263438
SoriaAlvaro <alvarosoria.universidad@gmail.com>
2022-11-09 20:05:09 -0300

    funca hasta ahora

--- CadeteriaBugMan/Controllers/CadeteController.cs
+++ CadeteriaBugMan/Controllers/CadeteController.cs
@@ -5,12 +5,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-
+using CadeteriaBugMan.Models;
 namespace CadeteriaBugMan.Controllers
 {
     public class CadeteController : Controller
     {
+        static List<Cadete> listaCadetes = new List<Cadete>();
         public IActionResult Index()
+        {
+            return View(listaCadetes);
+        }
+        [HttpPost]
+        public IActionResult GuardarCadete(Cadete cadete) {
+            cadete.Id++;
+            ViewData["IdCadet"] = cadete.Id;
+            listaCadetes.Add(cadete);
+            return RedirectToAction("Index",listaCadetes);
+        }
+        public IActionResult AltaCadete()
         {
             return View();
         }
--- CadeteriaBugMan/wwwroot/js/site.js
+++ CadeteriaBugMan/wwwroot/js/site.js
@@ -1,4 +1,24 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const d = document;
+let down = "▼",
+up = "▲";
+const toggleList = (panel) =>{
+    const allPanel = d.querySelectorAll(panel);
+    allPanel.forEach(p => {
+        p.addEventListener("click", e => {
+            const padre = e.target.parentNode;
+            if(e.target.matches(panel)){
+                padre.children[1].classList.toggle("open-panel--info");
+                if(p.children[1].textContent == down){
+                    p.children[1].textContent = up;
+                }else {
+                    p.children[1].textContent = down;
+                }
+            }
+        });
+    });
+}
+d.addEventListener("DOMContentLoaded", e => {
+    toggleList(".panel");
+});
 
-// Write your JavaScript code.
+
